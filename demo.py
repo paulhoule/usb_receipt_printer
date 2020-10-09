9f6fb61d378d89d35ca244497af3c5b284c2280b
paul <paul.houle@ontology2.com>
2020-10-08 22:13:56 -0400

    demo program tries to draw image

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
 from escpos.printer import Usb
+from pathlib import Path
+image = Path("/tamamo-no-mae/simple-1.png")
 printer = Usb(0x0416, 0x5011, 0, profile="ZJ-5870")
-printer.text("this is A test")
+printer.image(image);
 printer.cut()
 
 # with printer() as that:
